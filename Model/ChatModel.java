69ea44128ba10c8b12dcc5e85aee32b61dbbc875
Sumit Ahir <SumitAhir@users.noreply.github.com>
2015-09-04 23:03:37 +0530

    Update ChatModel.java

--- Model/ChatModel.java
+++ Model/ChatModel.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package chat.Model;
 
 import java.io.DataOutputStream;
@@ -14,7 +10,7 @@ import javafx.beans.property.StringProperty;
 
 /**
  *
- * @author Sam
+ * @author Sumit Ahir
  */
 public class ChatModel {
 
